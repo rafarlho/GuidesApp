8cabb756e8a1787ad73df1b83e36fae06a571d8d
rafarlho <rjfcarvalho@ua.pt>
2024-09-16 11:09:41 +0100

    basic implementation

--- GuidesApp/src/app/app.component.ts
+++ GuidesApp/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatIconModule,
+    MatToolbarModule, 
+    MatButtonModule,
+    MatSidenavModule, 
+    MatFormFieldModule, 
+    MatSelectModule, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
